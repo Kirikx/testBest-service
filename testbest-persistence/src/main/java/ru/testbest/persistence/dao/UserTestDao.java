97ead792b0ca9ff9777d48f0a105a43fd9e48069
Kirill Semenov <kirikx@mail.ru>
2021-03-18 20:53:37 +0300

    feature/add_converter_dto - begin work UserTestService

--- testbest-persistence/src/main/java/ru/testbest/persistence/dao/UserTestDao.java
+++ testbest-persistence/src/main/java/ru/testbest/persistence/dao/UserTestDao.java
@@ -1,10 +1,15 @@
 package ru.testbest.persistence.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.testbest.persistence.entity.UserTest;
 
 @Repository
 public interface UserTestDao extends JpaRepository<UserTest, String> {
+
+  List<UserTest> findAllByUserId(String userId);
+
+  List<UserTest> findAllByUserIdAndFinishedIsNull(String userId);
 
 }
--- testbest-service/src/main/java/ru/testbest/service/UserTestService.java
+++ testbest-service/src/main/java/ru/testbest/service/UserTestService.java
@@ -1,11 +1,16 @@
 package ru.testbest.service;
 
 import java.util.List;
+import ru.testbest.dto.test.QuestionDto;
 import ru.testbest.dto.test.UserTestDto;
+import ru.testbest.dto.test.UserTestQuestionDto;
 
 public interface UserTestService {
 
-  List<UserTestDto> getUserTests();
-//  List
+  List<UserTestDto> getUserTests(String userId);
+
+  List<QuestionDto> startUserTestByTestId(String testId);
+
+  QuestionDto createUserAnswer(UserTestQuestionDto userTestQuestionDto);
 
 }
