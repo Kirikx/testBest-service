0fd39a1f660613067029b1fb2f3e6c4e097bfb98
ABolikov <Brolond@mail.ru>
2021-03-21 16:15:54 +0600

    Перенос модуля front в модуль app. Со своей серверной частью.

--- testbest-app/karma.conf.js
+++ testbest-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/testbest-front'),
+      dir: require('path').join(__dirname, './coverage/testbest-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
