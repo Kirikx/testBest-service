350b9570f69e81d9c9e687e861fabd065dfbc004
ABolikov <Brolond@mail.ru>
2021-03-25 00:24:38 +0600

    Добавление продовского стенда

---
+++ testbest-app/src/app/global.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Globals {
+  static PROD: string = 'http://devstend.ru:8080';
+  //static PROD: string = 'http://localhost:8080';
+}
