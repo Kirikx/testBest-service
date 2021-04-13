7f127244c1694e2dd4f31d302612dd4a3b604664
Kirill Semenov <kirikx@mail.ru>
2021-04-13 12:59:15 +0300

    feature/form_admin - edit UserTestRestController

--- testbest-app/src/main/java/ru/testbest/security/WebSecurityConfig.java
+++ testbest-app/src/main/java/ru/testbest/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.cors().and().csrf().disable()
       .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-      .authorizeRequests().antMatchers("/api/**").permitAll()
+      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
       .antMatchers(AUTH_SWAGGER_WHITELIST).permitAll()
       .anyRequest().authenticated();
 
